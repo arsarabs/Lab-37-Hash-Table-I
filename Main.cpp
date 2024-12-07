3c8d17bdbfceabeebf0f408064a52b3522cd9de2
Your Name <your-email@example.com>
2024-12-07 03:39:13 -0800

    wip

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+//STEP 1a: Function Prototypes
+//-we need  sum_ascii() that receives a single string and returns the sum of that string's character's ASCII values. 
+int sum_ascii(const string& input);
 
 
 int main() {
@@ -21,3 +26,11 @@ These targets are present in the dataset and can be used for testing:
 666D109AA22E
 E1D2665B21EA
 */
+//STEP 1b: Function Defintions
+//-we need  sum_ascii() that receives a single string and returns the sum of that string's character's ASCII values. 
+int sum_ascii(const string& input) {
+    int total = 0;
+    for (char c : input) {
+        total += static_cast<int>(c); // this willl cast char to int 
+    }
+}
