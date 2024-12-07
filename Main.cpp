6e6e4049174f5d725cca97d7d14847172a93730b
Your Name <your-email@example.com>
2024-12-07 04:00:32 -0800

    End of part 1

--- Main.cpp
+++ Main.cpp
@@ -18,20 +18,29 @@ int main() {
     cout << "----------------------------------------------" << endl;
     //Declare/Initialize Variables for testing
     string test1 = "536B9DFC93AF";
-    string test2 = "536B9DFC93AF";
-    string test3 = "536B9DFC93AF";
-    string test4 = "536B9DFC93AF";
+    string test2 = "1DA9D64D02A0";
+    string test3 = "666D109AA22E";
+    string test4 = "E1D2665B21EA";
+
+    cout << "just testing...." << endl;
+
+    cout << "Sum of ASCII values in \"" << test1 << "\": "
+         << sum_ascii(test1) << endl; // expecting 729
+
+    cout << "Sum of ASCII values in \"" << test2 << "\": "
+        << sum_ascii(test2) << endl;  // expecting 692
+
+
+    cout << "Sum of ASCII values in \"" << test3 << "\": "
+        << sum_ascii(test3) << endl; // expecting 683
+
+
+    cout << "Sum of ASCII values in \"" << test4 << "\": "
+        << sum_ascii(test4) << endl;  // expecting 696
 
     return 0;
 }
-
-/*
-These targets are present in the dataset and can be used for testing:
-536B9DFC93AF
-1DA9D64D02A0
-666D109AA22E
-E1D2665B21EA
-*/
+ 
 //STEP 1b: Function Defintions
 //-we need  sum_ascii() that receives a single string and returns the sum of that string's character's ASCII values. 
 int sum_ascii(const string& input) {
@@ -39,4 +48,5 @@ int sum_ascii(const string& input) {
     for (char c : input) {
         total += static_cast<int>(c); // this willl cast char to int 
     }
+    return total; //forgot to return total haha
 }
